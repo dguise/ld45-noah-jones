943b0d1bb7034cdeeb21ddb7b667862e1698743a
Magnus Tullberg <magnus.tullberg@gmail.com>
2019-10-06 14:49:28 +0200

    add game over scene and win condition

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -53,7 +55,7 @@ public class GameManager : MonoBehaviour
         score += amount;
         if (score >= animals)
         {
-            Debug.Log("Win!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
