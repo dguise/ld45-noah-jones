fa3ca243248d1f9540c15a304a336d91771a81c6
Magnus Tullberg <magnus.tullberg@gmail.com>
2019-10-06 17:37:28 +0200

    add sad (but realistic) ending :feelsbadmil:

---
+++ Assets/LowPolyWater_Pack/Expand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expand : MonoBehaviour
+{
+    public float scaleSpeed = 0.001f;
+    void FixedUpdate()
+    {
+        var scale = new Vector3(scaleSpeed, 0, scaleSpeed);
+        transform.localScale = transform.localScale + scale * Time.deltaTime;
+    }
+}
