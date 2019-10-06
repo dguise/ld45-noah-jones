79ef8a245d9be3725822036126fa337a9173a3f1
Maakep <schon.fredrik@gmail.com>
2019-10-06 02:27:34 +0200

    Add day & night time from Sun

--- Assets/Terrain/Sun.cs
+++ Assets/Terrain/Sun.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Sun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 startingPosition = new Vector3(345.8f, 338.9f, -1000);
+    Vector3 endPosition = new Vector3(345.8f, 338.9f, 1127);
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, 1);
+
+        if (Vector3.Distance(this.transform.position, endPosition) < 10)
+        {
+            transform.position = startingPosition;
+        }
     }
 }
